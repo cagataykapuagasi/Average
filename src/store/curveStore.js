8d09293aab72a35586cba429385f13347cfc7a84
Cagatay Kapuagasi <cagatay.kapuagasi@gmail.com>
2019-06-05 18:55:25 +0300

    new store

---
+++ src/store/curveStore.js
@@ -0,0 +1,31 @@
+import { observable, action } from 'mobx';
+
+export default class curveStore {
+
+    @observable grade;
+
+    @observable average;
+
+    @observable sD;
+
+    constructor(rootStore) {
+        this.rootStore = rootStore;
+    }
+
+    @action getGrade = (value) => {
+        this.grade = value;
+    }
+
+    @action getAverage = (value) => {
+        this.average = value;
+    }
+
+    @action getSD = (value) => {
+        this.sD = value;
+    }
+
+
+
+
+
+}
