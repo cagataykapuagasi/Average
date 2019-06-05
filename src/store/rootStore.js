b8df5ba47cc40dc9e993a2448f9bef96dcf4b93c
Cagatay Kapuagasi <cagatay.kapuagasi@gmail.com>
2019-06-05 18:55:45 +0300

    add new store

--- src/store/rootStore.js
+++ src/store/rootStore.js
@@ -1,9 +1,11 @@
 import mainStore from './mainStore';
+import curveStore from './curveStore';
 
 class rootStore { 
 
     constructor() {
         this.mainStore = new mainStore(this);
+        this.curveStore = new curveStore(this);
     }
 
 }
