4f622f9c399882113da64bb8e7a04a288abc558a
Cagatay Kapuagasi <cagatay.kapuagasi@gmail.com>
2019-06-18 21:52:26 +0300

    create utils/grades

---
+++ src/util/grades.js
@@ -0,0 +1,17 @@
+export const letterGrade = [
+  {value:'AA'},
+  {value:'BA'},
+  {value:'BB'},
+  {value:'CB'},
+  {value:'CC'},
+  {value:'DC'},
+  {value:'DD'},
+  {value:'FD'},
+  {value:'FF'},
+];
+
+
+
+export default {
+  letterGrade,
+}
