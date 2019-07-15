623847daecc58f7c84604d4fc8f1e1521c68da32
Cagatay Kapuagasi <cagatay.kapuagasi@gmail.com>
2019-07-15 18:26:01 +0300

    Create podfile

--- App.js
+++ App.js
@@ -1,57 +1,64 @@
-import React, { Component } from 'react';
-import { StyleSheet } from 'react-native';
-import { Scene, Router, Stack, Tabs } from 'react-native-router-flux';
-import { Provider, observer } from 'mobx-react';
-import rootStore from './src/store/rootStore';
-import { MainScreen, BellCurveScreen, SettingsScreen, AverageScreen } from './src/screens';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import { colors } from './assets';
+import React, { Component } from "react";
+import { StyleSheet } from "react-native";
+import { Scene, Router, Stack, Tabs } from "react-native-router-flux";
+import { Provider, observer } from "mobx-react";
+import { store } from "./src/store";
+import { Main, BellCurve, Settings, Average } from "./src/screens";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { colors } from "./assets";
 
 class TabIcon extends Component {
-	render() {
-		const color = this.props.focused ? 'white' : 'gray';
-		return <Icon name={this.props.iconName || 'user-o'} size={30} color={color} />;
-	}
+  render() {
+    const color = this.props.focused ? "white" : "gray";
+    return (
+      <Icon name={this.props.iconName || "user-o"} size={30} color={color} />
+    );
+  }
 }
 
 export default class App extends Component {
-	render() {
-		return (
-			<Provider rootStore={rootStore}>
-				<Router tabBarStyle={styles.tab} backButtonTextStyle={styles.backButtonStyle} backButtonTintColor={"white"} sceneStyle={styles.sceneStyle} titleStyle={styles.titleStyle} navigationBarStyle={styles.navigationBarStyle} wrapBy={observer}>
-					<Tabs showLabel={false}>
-						<Stack iconName="tasks" icon={TabIcon} key="root">
-							<Scene initial component={MainScreen} hideNavBar key="main" />
-							<Scene
-								key="average"
-								component={AverageScreen}
-							/>
-						</Stack>
+  render() {
+    return (
+      <Provider store={store}>
+        <Router
+          tabBarStyle={styles.tab}
+          backButtonTextStyle={styles.backButtonStyle}
+          backButtonTintColor={"white"}
+          sceneStyle={styles.sceneStyle}
+          titleStyle={styles.titleStyle}
+          navigationBarStyle={styles.navigationBarStyle}
+          wrapBy={observer}
+        >
+          <Tabs showLabel={false}>
+            <Stack iconName="tasks" icon={TabIcon} key="root">
+              <Scene initial component={Main} hideNavBar key="main" />
+              <Scene key="average" component={Average} />
+            </Stack>
 
-						<Stack iconName="bell" icon={TabIcon} hideNavBar key="can">
-							<Scene component={BellCurveScreen} />
-						</Stack>
+            <Stack iconName="bell" icon={TabIcon} hideNavBar key="can">
+              <Scene component={BellCurve} />
+            </Stack>
 
-						<Stack iconName="cogs" icon={TabIcon} hideNavBar key="settings">
-							<Scene component={SettingsScreen} />
-						</Stack>
-					</Tabs>
-				</Router>
-			</Provider>
-		);
-	}
+            <Stack iconName="cogs" icon={TabIcon} hideNavBar key="settings">
+              <Scene component={Settings} />
+            </Stack>
+          </Tabs>
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-	tab: {
-		backgroundColor: '#2d2d2d'
-	},
+  tab: {
+    backgroundColor: "#2d2d2d"
+  },
   navigationBarStyle: {
-    backgroundColor : colors.background,
-    borderBottomWidth: 0,
+    backgroundColor: colors.background,
+    borderBottomWidth: 0
   },
   titleStyle: {
-    color: 'white',
+    color: "white"
   },
   sceneStyle: {
     backgroundColor: colors.background
--- android/app/src/main/java/com/ortalama/MainApplication.java
+++ android/app/src/main/java/com/ortalama/MainApplication.java
@@ -3,6 +3,7 @@ package com.ortalama;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AsyncStoragePackage(),
             new LinearGradientPackage(),
             new VectorIconsPackage()
       );
